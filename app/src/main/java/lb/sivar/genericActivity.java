8bac6c9cef49443f32787dd010c7e47655931b86
so-ph-ie <gorrolb@gmail.com>
2017-07-03 11:41:21 -0600

    ->GenericActivity

---
+++ app/src/main/java/lb/sivar/genericActivity.java
@@ -0,0 +1,17 @@
+package lb.sivar;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by HP on 3/7/2017.
+ */
+
+public class genericActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.generic_activity);
+
+    }
+}
